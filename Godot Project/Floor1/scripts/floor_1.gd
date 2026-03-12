078cb291343205f36f4be5976508947a6dc65312
Kingslayer <williamleva@live.com>
2026-03-11 21:08:00 -0400

    Room Generation Prototype

--- Godot Project/Floor1/scripts/floor_1.gd
+++ Godot Project/Floor1/scripts/floor_1.gd
@@ -1,18 +1,48 @@
 extends Node2D
 @onready var kunai = load("res://misc/scenes/kunai.tscn")
 @onready var ronins = load_ronin("res://Ronins/scenes/")
+@onready var normal_rooms = load_rooms("res://Floor1/scenes/Rooms/Normal/")
 
-var ronin_spawn = Vector2(10, 170)
+var ronin_spawn = Vector2(0, 10)
+var rng = RandomNumberGenerator.new()
 
 func _ready():
+	room_generation()
 	var ronin = load(ronins.pick_random()).instantiate()
 	var camera = Camera2D.new()
 	camera.make_current()
 	ronin.spawn_position = ronin_spawn
 	ronin.add_child(camera)
 	add_child.call_deferred(ronin)
+	
+func room_generation():
+	rng.randomize()
+	var rooms = []
+	var room_w = 480
+	var room_h = 240
+	rooms.push_back(load("res://Floor1/scenes/Rooms/Room_Start.tscn").instantiate())
+	add_child.call_deferred(rooms[0])
+	for i in range(1, rng.randi_range(3, 7)):
+		rooms.push_back(load(normal_rooms.pick_random()).instantiate())
+		rooms[i].position.x = room_w * i
+		add_child.call_deferred(rooms[i])
+	rooms.push_back(load("res://Floor1/scenes/Rooms/Room_End.tscn").instantiate())
+	rooms[rooms.size()-1].position.x = room_w * rooms.size()-1
+	add_child.call_deferred(rooms[rooms.size()-1])
 
 func load_ronin(path: String) -> Array:
+	var files = []
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir() and file_name.ends_with(".tscn"):
+				files.append(path.path_join(file_name))
+			file_name = dir.get_next()
+	return files
+
+func load_rooms(path: String) -> Array:
 	var files = []
 	var dir = DirAccess.open(path)
 	if dir:
